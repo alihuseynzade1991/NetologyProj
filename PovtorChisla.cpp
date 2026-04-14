9c7b3cf97e10aba449f668c56fc96fde8fa0d3ea
alihuseynzade1991 <alihuseynzade1991@outlook.com>
2026-04-14 18:41:15 +0400

    Add files via upload

---
+++ PovtorChisla.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main () {
+    int number;
+
+    std::cout << "Vvedite chislo: ";
+    std::cin >> number;
+
+    std::cout << "Vi vveli: " << number;
+
+    return 0;
+}
---
+++ PovtorSlovo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string word;
+
+    std::cout << "Vvedite slovo: ";
+    std::cin >> word;
+
+    std::cout << "Vi vveli: " << word << std::endl;
+}
